c3c62883db9f9f7f5da78d918c9c44be8aaba7b4
GiovanniTheo <112666306+GiovanniTheo@users.noreply.github.com>
2022-12-15 20:37:29 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+TOTAL=(0)
+CONTADOR_NOTAS=(0)
+user_input=(0)
+#definindo as variáveis TOTAL, CONTADOR_NOTAS e as das entradas do usuário (user_input), como zero
+while CONTADOR_NOTAS <10:
+#criando um while para fazer a somatória e divisão das notas entradas pelo usuário
+    try:
+#utilizando um try para controlar a entrada do usuário, ao digitar algo que não seja número no terminal
+        user_input =int(input("Digite a nota:"))
+        #definindo a entrada do usuário
+        if user_input > 10:
+            print("Coloque uma nota com o valor entre 0 e 10")
+        elif user_input < 0:
+            print("Coloque uma nota com o valor entre 0 e 10")
+        else:
+        #colocando exceções e fazendo o controle das entradas do usuário utilizando if, elif e else
+            TOTAL=TOTAL+user_input
+            CONTADOR_NOTAS=CONTADOR_NOTAS+1
+        #somando as notas entradas pelo usuário
+    except:
+        print("coloque um valor numérico entre 0 e 10")
+    #caso não tenha se entrado com valores adequador o except entrará em ação
+TOTAL=TOTAL/10
+#fazendo o calculo da média obtida pelo usuário
+print("Média:",TOTAL)
